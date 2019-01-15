e130e7dfa1d5d097fe0ad689d8fecb3e6dcb756c
JJ <jenjones@ucdavis.edu>
2019-01-15 13:08:16 -0800

    basic setup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Trivia Game
 
-## Random Questions for Fun
+## Interesting Random Trivia
 
 The game begins with a timer counting down from __ seconds.
---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+var question = "";
+var answer1 = "";
+var answer2 = "";
+var answer3 = "";
+var answer4 = "";
+
+// Start with title of game at the top and a start button.
+// Press start, title stays at the top of the page, 
+// replace button with "Time remaining:" with # of seconds left on line 1,
+// first question on line 2, with 4 possible answers (buttons).
+// Clicking an answer freezes timer, game tells player correct or not, 
+// and gives correct answer for # seconds.
+// Game goes to next question with new time remaining countdown.
+// If time runs out, display "out of time" message on line 1, 
+// correct answer on line 2 for # seconds.
+// At end of game, tell player "You are done! Here's your score:"
+// Correct Answers:  #, Incorrect Answers:  #, Unanswered:  #, Start Over? button
+// Clicking start over button resets game
+})
