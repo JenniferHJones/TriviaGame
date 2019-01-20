2f4f2a03a5452f5f52760554e2dd2fa2126859cd
JJ <jenjones@ucdavis.edu>
2019-01-20 11:10:11 -0800

    change question from individual variables to object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,66 +7,58 @@ $(document).ready(function () {
 
 
     // question variables
-    var question0 = {
+    var questions = [
+    {
         question: "A group of ravens is called...",
         answer: "An unkindness",
         choices: ["A murder", "An unkindness"],
-        correctAnswer: 1,
-    };
-    var question1 = {
+    },
+    {
         question: "The fortune cookie was invented in...",
         answer: "America",
         choices: ["America", "China"],
-        correctAnswer: 0,
-    };
-    var question2 = {
+    },
+    {
         question: "The plastic covering on the tip of a shoelace is called...",
         answer: "An aglet",
         choices: ["A grommet", "An aglet"],
-        correctAnswer: 1,
-    };
-    var question3 = {
+    },
+    {
         question: "This animal is known to kill more people than plane crashes…",
         answer: "Donkey",
         choices: ["Rattlesnake", "Donkey"],
-        correctAnswer: 1,
-    };
-    var question4 = {
+    },
+    {
         question: "The Great Wall of China is visible from outer space.",
         answer: "False",
         choices: ["False", "True"],
-        correctAnswer: 0,
-    };
-    var question5 = {
+    },
+    {
         question: "'The quick brown fox jumps over a lazy dog' is famous because…",
         answer: "It contains every letter of the English alphabet.",
         choices: ["It appears in a popular children’s book.", "It contains every letter of the English alphabet."],
-        correctAnswer: 1,
-    };
-    var question6 = {
+    },
+    {
         question: "The most popular boy’s first name in the world is…",
         answer: "Muhammed",
         choices: ["David", "Muhammed"],
-        correctAnswer: 1,
-    };
-    var question7 = {
+    },
+    {
         question: "Canine is to dogs as ursine is to...",
         answer: "Bears",
         choices: ["Bears", "Pigs"],
-        correctAnswer: 0,
-    };
-    var question8 = {
+    },
+    {
         question: "Espadrilles are...",
         answer: "Sandals",
         choices: ["Sandals", "A savory snack"],
-        correctAnswer: 0,
-    };
-    var question9 = {
+    },
+    {
         question: "Titan is this planet's largest moon...",
         answer: "Saturn",
         choices: ["Jupiter", "Saturn"],
-        correctAnswer: 1,
-    };
+    },
+]
 
     var questions = [question0, question1, question2, question3, question4, question5, question6, question7, question8, question9];
 
